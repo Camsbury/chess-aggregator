0c7b9a67b1e6b58b7c124b7158ce2e0679c8c631
Cameron Kingsbury <camsbury7@gmail.com>
2023-01-26 11:21:15 -0500

    wip: switch to shakmaty

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 extern crate chess;
 extern crate pgn_reader;
 extern crate rocksdb;
+extern crate shakmaty;
 extern crate sysinfo;
 extern crate zstd;
 
-use chess::{Board, ChessMove, Game, Piece, Rank, Square};
 use pgn_reader::{BufferedReader, Color, Outcome, SanPlus, Skip, Visitor};
 use rocksdb::{Options, WriteBatch, DB};
+use shakmaty::{fen::Fen, uci::Uci, CastlingMode, Chess, Move, Position};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::str::FromStr;
 use sysinfo::{System, SystemExt};
 use zstd::stream::read::Decoder;
 
@@ -35,21 +35,6 @@ fn main() {
     let mut batch = WriteBatch::default();
 
     let mut sys = System::new_all();
-
-    // {
-    //     let position = Board::from_str(
-    //         "2rr2k1/5pp1/p3p2p/P7/RQ1q4/1P4P1/2p2P1P/1N2R1K1 b - - 0 1",
-    //     )
-    //     .ok()
-    //     .unwrap();
-    //     match ChessMove::from_san(&position, "c1Q") {
-    //         Ok(chess_move) => println!("UCI of c1Q: {}", chess_move),
-    //         Err(err) => {
-    //             println!("Error: {:?}", err);
-    //             std::process::exit(1);
-    //         }
-    //     }
-    // }
 
     let reader = BufReader::new(file);
     for line in reader.lines() {
@@ -77,11 +62,15 @@ fn main() {
 
     ////////////////////////////////////////////////////////////////////////////
     // NOTE: Testing that the db is actually being written to
-    let start_pos = Board::from_str(
-        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-    )
-    .ok()
-    .unwrap();
+    let start_fen: Fen =
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+            .parse()
+            .ok()
+            .unwrap();
+    let start_pos: Chess = start_fen
+        .into_position(CastlingMode::Standard)
+        .ok()
+        .unwrap();
 
     let start_pos_count = fetch_pos_count(&db, &start_pos);
 
@@ -96,7 +85,7 @@ fn main() {
 }
 
 struct MyVisitor<'a> {
-    board: Board,
+    board: Chess,
     outcome: Option<Outcome>,
     db: &'a DB,
     batch: &'a mut WriteBatch,
@@ -112,7 +101,7 @@ impl MyVisitor<'_> {
         sys: &'a mut System,
     ) -> MyVisitor<'a> {
         MyVisitor {
-            board: Board::default(),
+            board: Chess::default(),
             outcome: None,
             db,
             batch,
@@ -127,7 +116,7 @@ impl Visitor for MyVisitor<'_> {
     type Result = ();
 
     fn begin_game(&mut self) {
-        self.board = Board::default();
+        self.board = Chess::default();
         self.outcome = None;
         self.game_count += 1;
         self.move_count = 0;
@@ -139,25 +128,21 @@ impl Visitor for MyVisitor<'_> {
 
     fn san(&mut self, san_plus: SanPlus) {
         self.move_count += 1;
-        match ChessMove::from_san(
-            &self.board,
-            &san_plus.san.to_string().replace('=', ""),
-        ) {
+        match san_plus.san.to_move(&self.board) {
             Ok(chess_move) => {
                 write_pos_info(
                     self.db,
                     self.batch,
                     self.sys,
-                    self.board,
+                    &self.board,
                     self.outcome,
-                    Some(chess_move),
+                    Some(&chess_move),
                 );
-                let mut result = Board::default();
-                self.board.make_move(chess_move, &mut result);
-                if let Some(square) = result.en_passant() {
-                    result.en_passant = fix_en_passant(square)
+
+                match std::mem::take(&mut self.board).play(&chess_move) {
+                    Ok(new_board) => self.board = new_board,
+                    Err(err) => panic!("{}", err),
                 }
-                self.board = result
             }
             Err(err) => {
                 println!(
@@ -167,7 +152,7 @@ impl Visitor for MyVisitor<'_> {
                      Game Count: {}
                      Move Count: {}",
                     err,
-                    self.board,
+                    self.board.board(),
                     &san_plus.san.to_string(),
                     self.game_count,
                     self.move_count,
@@ -185,7 +170,7 @@ impl Visitor for MyVisitor<'_> {
             self.db,
             self.batch,
             self.sys,
-            self.board,
+            &self.board,
             self.outcome,
             None,
         );
@@ -196,53 +181,55 @@ fn write_pos_info(
     db: &DB,
     batch: &mut WriteBatch,
     sys: &mut System,
-    position: Board,
+    position: &Chess,
     outcome: Option<Outcome>,
-    chess_move: Option<ChessMove>,
+    chess_move: Option<&Move>,
 ) {
-    let pc_key = create_pc_key(&position);
+    let pc_key = create_pc_key(position);
     increment_key(db, batch, sys, pc_key.as_slice());
 
     if let Some(Outcome::Decisive { winner: color }) = outcome {
         match color {
             Color::White => {
-                let pwc_key = create_pwc_key(&position);
+                let pwc_key = create_pwc_key(position);
                 increment_key(db, batch, sys, pwc_key.as_slice());
             }
             Color::Black => {
-                let pbc_key = create_pbc_key(&position);
+                let pbc_key = create_pbc_key(position);
                 increment_key(db, batch, sys, pbc_key.as_slice());
             }
         }
     }
     if let Some(m) = chess_move {
-        let puc_key = create_puc_key(&position, &m);
+        let puc_key = create_puc_key(position, m);
         increment_key(db, batch, sys, puc_key.as_slice());
     }
 }
 
-fn create_pc_key(position: &Board) -> Vec<u8> {
+fn create_pc_key(position: &Chess) -> Vec<u8> {
     let mut pc_vec = "pos_count".as_bytes().to_vec();
-    pc_vec.extend_from_slice(position.get_hash().to_be_bytes().as_slice());
+    pc_vec.extend_from_slice(position.board().to_string().as_bytes());
     pc_vec
 }
 
-fn create_pwc_key(position: &Board) -> Vec<u8> {
+fn create_pwc_key(position: &Chess) -> Vec<u8> {
     let mut pwc_vec = "pos_white_count".as_bytes().to_vec();
-    pwc_vec.extend_from_slice(position.get_hash().to_be_bytes().as_slice());
+    pwc_vec.extend_from_slice(position.board().to_string().as_bytes());
     pwc_vec
 }
 
-fn create_pbc_key(position: &Board) -> Vec<u8> {
+fn create_pbc_key(position: &Chess) -> Vec<u8> {
     let mut pbc_vec = "pos_black_count".as_bytes().to_vec();
-    pbc_vec.extend_from_slice(position.get_hash().to_be_bytes().as_slice());
+    pbc_vec.extend_from_slice(position.board().to_string().as_bytes());
     pbc_vec
 }
 
-fn create_puc_key(position: &Board, chess_move: &ChessMove) -> Vec<u8> {
+fn create_puc_key(position: &Chess, chess_move: &Move) -> Vec<u8> {
     let mut puc_vec = "pos_uci_count".as_bytes().to_vec();
-    puc_vec.extend_from_slice(position.get_hash().to_be_bytes().as_slice());
-    puc_vec.extend_from_slice(chess_move.to_string().as_bytes());
+    puc_vec.extend_from_slice(position.board().to_string().as_bytes());
+    puc_vec.extend_from_slice(
+        Uci::from_standard(chess_move).to_string().as_bytes(),
+    );
     puc_vec
 }
 
@@ -275,7 +262,7 @@ fn increment_key(
     }
 }
 
-fn fetch_pos_count(db: &DB, pos: &Board) -> u64 {
+fn fetch_pos_count(db: &DB, pos: &Chess) -> u64 {
     let key = create_pc_key(pos);
     let count: u64;
     match db.get_pinned(key.as_slice()) {
@@ -295,15 +282,4 @@ fn fetch_pos_count(db: &DB, pos: &Board) -> u64 {
         }
     }
     count
-}
-
-fn fix_en_passant(square: Square) -> Square {
-    match square.get_rank() {
-        Rank::Fifth => Square::make_square(Rank::Sixth, square.get_file()),
-        Rank::Fourth => Square::make_square(Rank::Third, square.get_file()),
-        _ => {
-            println!("Failed to fix en passant for: {}", square);
-            std::process::exit(1);
-        }
-    }
 }
