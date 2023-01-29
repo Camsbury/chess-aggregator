73dc82c66bc3babc3d39664e97c76900283bfc5f
Cameron Kingsbury <camsbury7@gmail.com>
2023-01-29 10:33:20 -0500

    wip

--- src/traversal.rs
+++ src/traversal.rs
@@ -1,17 +1,40 @@
 use crate::GameStats;
 use radix_trie::{Trie, SubTrie, TrieCommon};
+use shakmaty::Chess;
+
+type StatsTree = Trie<String, GameStats>;
+type StatsST<'a> = SubTrie<'a, String, GameStats>;
 
 struct TraversalStep<'a> {
-    tree: SubTrie<'a, String, GameStats>
+    tree: StatsST<'a>,
+    game_stack: Vec<Chess>,
+}
+
+impl TraversalStep<'_> {
+    fn new(tree: &StatsTree) -> TraversalStep {
+        Self::from_subtree(
+            tree.children().next().unwrap(),
+            vec![Chess::new()],
+        )
+    }
+
+    fn from_subtree(subtree: StatsST, game_stack: Vec<Chess>) -> TraversalStep {
+        TraversalStep {
+            tree: subtree,
+            game_stack,
+        }
+    }
 }
 
 pub fn extract_stats(tree: Trie<String, GameStats>) {
-    let subtrie = tree.children().next().unwrap();
-    let mut stack = vec![TraversalStep { tree: subtrie }];
+    let mut stack = vec![TraversalStep::new(&tree)];
     while !stack.is_empty() {
         let step = stack.pop().unwrap();
         for child in step.tree.children() {
-            stack.push(TraversalStep { tree: child });
+            stack.push(TraversalStep::from_subtree(
+                child,
+                vec![Chess::new()],
+            ));
         }
     }
 }
