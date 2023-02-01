af24835cddda09541f3511ac7be4fbd1e5f022e5
Cameron Kingsbury <camsbury7@gmail.com>
2023-02-01 14:26:50 -0500

    wip: debug key indexing

--- src/traversal.rs
+++ src/traversal.rs
@@ -53,15 +53,33 @@ impl TraversalStep<'_> {
     }
 }
 
-fn is_true_leaf(tree: &StatsST) -> bool {
-    let mut ret = true;
-    for _ in tree.children() {
-        ret = false;
+fn extract_san_strs(step: &TraversalStep, node: &StatsST) -> (Nibblet, usize, Vec<String>) {
+    let prefix = step.prefix_acc.clone().join(node.prefix());
+    let pre_offset_vec = &prefix.as_bytes().to_vec();
+    let prefix_vec = &prefix.as_bytes().to_vec();
+    let new_offset =
+        prefix_vec.iter().rposition(|x| *x == SEPARATOR);
+
+    // println!("Prefix: {prefix:?}");
+    // println!("Pre-offset Vec: {pre_offset_vec:?}");
+    // println!("Prefix Vec: {prefix_vec:?}");
+    // println!("Offset: {:?}", step.offset);
+    // println!("New Offset: {new_offset:?}");
+    if let Some(end) = new_offset {
+        if end > step.offset {
+            if let Ok(moves_string) = String::from_utf8(prefix_vec[step.offset..end].to_vec()) {
+                (prefix, end, moves_string.split_whitespace().map(|s| s.to_string()).collect())
+            } else {
+                (prefix, end, Vec::new())
+            }
+        } else {
+            (prefix, step.offset, Vec::new())
+        }
+    } else {
+        (prefix, step.offset, Vec::new())
     }
-    ret
 }
 
-//FIXME -- still lost on empty moves_string in the leaf case...
 pub fn extract_stats(db: &DB, tree: &mut Trie<String, GameWins>) {
     let tree = std::mem::take(tree); // Clearing out the tree for later use
     let mut batch = WriteBatch::default();
@@ -69,71 +87,50 @@ pub fn extract_stats(db: &DB, tree: &mut Trie<String, GameWins>) {
         Some(step) => vec![step],
         None => vec![],
     };
+
     while !stack.is_empty() {
+        //extract current step
         let step = stack
             .pop()
             .expect("should only loop if the stack has items");
-        if is_true_leaf(&step.tree) {
-            // println!("true leaf!!");
-            let prefix = step.prefix_acc.clone();
+        for child in step.tree.children() {
             let mut game_stack = step.game_stack.clone();
-
-            let moves_string = step
-                .tree
-                .key()
-                .expect("invalid key for tree?")
-                .chars()
-                .into_iter()
-                .skip(
-                    String::from_utf8(prefix.clone().into_bytes())
-                        .expect("Invalid utf8 for prefix")
-                        .chars()
-                        .count(),
-                )
-                .collect::<String>();
-
-            // println!("Prefix: {:?}",
-            //         String::from_utf8(prefix.clone().into_bytes())
-            //             .expect("Invalid utf8 for prefix"));
-            // println!("Key: {:?}", step.tree.key());
-            // println!("Moves string: {}", moves_string);
-            // println!("Games Stack: {:?}", game_stack);
-            for m_str in moves_string.split_whitespace() {
-                if let Some(Game {
-                    position: old_pos,
-                    game_move: _,
-                }) = game_stack.pop()
-                {
-                    let san_may: Result<San, _> = m_str.parse();
-                    if let Ok(san_move) = san_may {
-                        let move_may: Result<Move, _> =
-                            san_move.to_move(&old_pos.clone());
-                        if let Ok(m) = move_may {
-                            if let Ok(new_pos) = old_pos.clone().play(&m) {
-                                game_stack.push(Game {
-                                    position: old_pos,
-                                    game_move: Some(m),
-                                });
-                                game_stack.push(Game {
-                                    position: new_pos,
-                                    game_move: None,
-                                });
-                            } else {
-                                break;
-                            }
-                        } else {
-                            break;
-                        }
-                    } else {
-                        break;
-                    }
-                } else {
-                    break;
+            // println!("Child in loop: {:?}", child);
+            let (prefix, offset, san_strs) = extract_san_strs(&step, &child);
+            for san_str in san_strs.clone() {
+                let Game {position: old_pos, game_move: _} =
+                    game_stack.pop().expect("No step on top of game stack!!");
+                let san_move: San = san_str.parse().expect("Invalid SAN.");
+                if chess_db::pos_to_fen(old_pos.clone()) == "r1bqkb1r/pppp1ppp/2n2n2/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R w KQkq -" {
+                    // println!("old prefix: {:?}", step.prefix_acc);
+                    // println!("old offset: {:?}", step.offset);
+                    // println!("new prefix: {:?}", prefix);
+                    // println!("new offset: {:?}", offset);
+                    // println!("Moves: {:?}", san_strs);
+                    // println!("Move: {}", san_str);
+                    // println!("Next move: {}", String::from_utf8(vec![78, 102, 54]).unwrap());
+                }
+                let m = san_move.to_move(&old_pos).expect("Invalid position/move combo");
+                let new_pos = old_pos.clone().play(&m).expect("Invalid move for old pos!");
+                game_stack.push(Game {
+                    position: old_pos.clone(),
+                    game_move: Some(m),
+                });
+                game_stack.push(Game {
+                    position: new_pos.clone(),
+                    game_move: None,
+                });
+                if chess_db::pos_to_fen(new_pos.clone()) == "r1bqkb1r/pppp1ppp/2n2n2/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R w KQkq -" {
+                    // println!("old prefix: {:?}", step.prefix_acc);
+                    // println!("old offset: {:?}", step.offset);
+                    // println!("new prefix: {:?}", prefix);
+                    // println!("new offset: {:?}", offset);
+                    // println!("Moves: {:?}", san_strs);
+                    // println!("Move: {}", san_str);
                 }
             }
-
-            if let Some(game_stats) = step.tree.value() {
-                for game in game_stack.iter().rev() {
+            if let Some(game_stats) = child.value() {
+                for game in game_stack.iter() {
                     chess_db::update_pos_wins(
                         db,
                         &mut batch,
@@ -153,122 +150,13 @@ pub fn extract_stats(db: &DB, tree: &mut Trie<String, GameWins>) {
                 let old_batch = std::mem::take(&mut batch);
                 db.write(old_batch).expect("Batch couldn't write to DB");
             }
-        } else {
-            // iterate through children in trie
-            for child in step.tree.children() {
-                // build up the new moves
-                let prefix = step.prefix_acc.clone().join(child.prefix());
-                let prefix_vec = &prefix.as_bytes().to_vec()[step.offset..];
-                let new_offset =
-                    prefix_vec.iter().rposition(|x| *x == SEPARATOR);
-                let mut game_stack = step.game_stack.clone();
 
-                // TODO: write with and_then!
-                if let Some(end) = new_offset {
-                    if end > step.offset {
-                        if let Ok(moves_string) = String::from_utf8(
-                            // extract the new moves
-                            prefix_vec[..end].to_vec(),
-                        ) {
-                            // println!("Start index: {}", step.offset);
-                            // println!("End index: {}", end);
-                            // println!(
-                            //     "Last position: {}",
-                            //     chess_db::pos_to_fen(game_stack.last().unwrap().position.clone())
-                            // );
-                            // println!("Moves string: {}", moves_string);
-                            // println!(
-                            //     "Moves string unindexed: {}",
-                            //     String::from_utf8(prefix_vec.to_vec()).unwrap(),
-                            // );
-                            for m_str in moves_string.split_whitespace() {
-                                // pop the old game
-                                if let Some(Game {
-                                    position: old_pos,
-                                    game_move: _,
-                                }) = game_stack.pop()
-                                {
-                                    // parse the move
-                                    let san_may: Result<San, _> = m_str.parse();
-                                    if let Ok(san_move) = san_may {
-                                        // println!(
-                                        //     "Old pos: {}",
-                                        //     chess_db::pos_to_fen(old_pos.clone()),
-                                        // );
-                                        // println!("SAN move: {:?}", san_move);
-                                        if san_move.to_string() == "b6" {
-                                            println!(
-                                                "Game stack: {:?}",
-                                                game_stack,
-                                            );
-                                            println!(
-                                                "Old pos: {}",
-                                                chess_db::pos_to_fen(old_pos.clone()),
-                                            );
-                                        }
-                                        let move_may: Result<Move, _> =
-                                            san_move.to_move(&old_pos.clone());
-                                        if let Ok(m) = move_may {
-                                            // FIXME - doesn't match pos
-                                            // check that the move actually works
-                                            // println!("Accepted move: {:?}", m);
-                                            if let Ok(new_pos) =
-                                                old_pos.clone().play(&m)
-                                            {
-                                                game_stack.push(Game {
-                                                    position: old_pos,
-                                                    game_move: Some(m),
-                                                });
-                                                game_stack.push(Game {
-                                                    position: new_pos,
-                                                    game_move: None,
-                                                });
-                                            } else {
-                                                break;
-                                            }
-                                        } else {
-                                            break;
-                                        }
-                                    } else {
-                                        break;
-                                    }
-                                } else {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if let Some(game_stats) = child.value() {
-                    for game in game_stack.iter().rev() {
-                        chess_db::update_pos_wins(
-                            db,
-                            &mut batch,
-                            game.position.clone(),
-                            *game_stats,
-                        );
-                        if let Some(m) = game.game_move.clone() {
-                            chess_db::update_pos_move_wins(
-                                db,
-                                &mut batch,
-                                game.position.clone(),
-                                m,
-                                *game_stats,
-                            )
-                        }
-                    }
-                    let old_batch = std::mem::take(&mut batch);
-                    db.write(old_batch).expect("Batch couldn't write to DB");
-                }
-
-                stack.push(TraversalStep::build_step(
-                    child,
-                    game_stack,
-                    prefix,
-                    new_offset.unwrap_or(step.offset),
-                ));
-            }
+            stack.push(TraversalStep::build_step(
+                child,
+                game_stack.clone(),
+                prefix,
+                offset,
+            ));
         }
     }
     db.write(batch).expect("Batch couldn't write to DB");
